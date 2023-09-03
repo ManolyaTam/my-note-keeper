3ad0130d89db4be417ccd77caa25475deb7b816c
Manolya Tamimi <tamimimanolya@gmail.com>
2023-09-03 20:16:38 +0300

    connecting server to mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const mongoose = require('mongoose');
+const express = require('express');
 const app = new express();
 
 app.get('/notes', (req, res) => {
@@ -19,4 +20,16 @@ app.put('/notes/:id', (req, res) => {
     res.send(`updating note #${id}`)
 })
 
-app.listen(3001, () => console.log('listening on port 3001'))
+app.listen(3001, () => {
+    console.log('Listening on port 3001');
+    dbConnect();
+})
+
+const dbConnect = () => {
+    mongoose.connect('mongodb://127.0.0.1/my-note-keeper')
+        .then(() => console.log('Connceted to MongoDB'))
+        .catch((err) => {
+            console.log('An error occured while connecting to the Database\n', err);
+            return;
+        })
+}
